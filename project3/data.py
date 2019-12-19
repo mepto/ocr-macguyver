621fb3cc1b538d6427404133992983cef563fb1d
GERST Nathalie <NGERST@ad.edqm.eu>
2019-12-19 17:13:42 +0100

    Retrieve zone type

--- project3/data.py
+++ project3/data.py
@@ -1,26 +1,17 @@
 #! /usr/bin/venv python3
 # coding: utf-8
 
-import pygame as pg
+"""
+:synopsis: [...]
+"""
 
+# Standard lib imports
+import sys
 import json
 from random import randint
 
-
-class Human:
-
-    def __init__(self, position=(1, 1), image="https://avatars.dicebear.com/v2/male/joe.svg"):
-        self.is_alive = True
-        self.victory_phrase = "Yay"
-        self.failure_phrase = "Argh..."
-        self.position = position
-        self.image = image
-
-
-class Hero(Human):
-    def __init__(self, position, image):
-        super().__init__(position, image)
-        self.items = 0
+# Third-party imports
+import pygame as pg
 
 
 class Background:
@@ -41,47 +32,72 @@ class Background:
                              "https://cdn3.iconfinder.com/data/icons/glypho-free/64/flask-256.png")
         self.macguyver = Hero((0, 13), "https://user.oc-static.com/upload/2017/04/21/14927753100739_macgyver-32-43.png")
         self.villain = Human((14, 6), "https://user.oc-static.com/upload/2017/04/21/14927753225921_murdoc-32.png")
-        self.moves = 0
 
     def display_background(self, values):
         # values_pd = pd.DataFrame(self.read_values_from_json(values))
         values = self.read_values_from_json(values)
         # print(values_pd)
-        print(values)
+        print(self.VALUES)
 
     def read_values_from_json(self, level_nb):
-
+        """ Retrieve background data from json file"""
         # check if file exists
         try:
             open(self.FILE_LEVELS)
         except (OSError, IOError) as e:
-            # print("error no.: " + str(e.errno))
-            self.VALUES = "Cannot create background: no file found to create levels."
+            self.VALUES.append("Cannot create background: no file "
+                               "found to create levels (error no.: " +
+                               str(e.errno) + ").")
+        except Exception:
+            self.VALUES.append("Cannot create background, "
+                               "an unexpected error occurred:"
+                               + str(sys.exc_info()[0]))
         else:
             # open a json file with my objects
             with open(self.FILE_LEVELS) as level:
+                self.VALUES = []
                 # load all the data contained in this file. data = entries
-                # data = json.load(level)
-                # self.VALUES.append(data[level_nb])
                 self.VALUES.append(json.load(level)[level_nb])
 
         return self.VALUES
 
     def randomize_position(self):
+        """ Sets a random position to Items MacGuyver needs to find """
         column = randint(1, 15)
         row = randint(1, 15)
-        while self.is_colliding(self.__class__.__name__, (column, row)):
+        while self.is_colliding("Item", (column, row)):
             column = randint(1, 15)
             row = randint(1, 15)
         else:
             return column, row
 
-    def is_colliding(self, class_name, instance_position):
-        # print(instance_position)
-        if instance_position == "(1, 1)":
-            return True
-        else:
-            return False
+    def is_colliding(self, instance, position):
+        """ Verifies if position of objects are relevant relative
+        to each other and background elements """
+        col = position[0] - 1
+        row = position[1] - 1
+        print(instance)
+        zone_type = self.VALUES[0][col][row]
+        return False
+
+
+class Human:
+
+    def __init__(self, position=(1, 1),
+                 image="https://avatars.dicebear.com/v2/male/joe.svg"):
+        self.is_alive = True
+        self.victory_phrase = "Yay"
+        self.failure_phrase = "Argh..."
+        self.position = position
+        self.image = image
+
+
+class Hero(Human):
+
+    def __init__(self, position, image):
+        super().__init__(position, image)
+        self.items = 0
+        self.moves = 0
 
 
 class NewItem:
@@ -92,7 +108,8 @@ class NewItem:
         self.display_item()
 
     def display_item(self):
-        print(self.position)
+        pass
+        # print(self.position)
 
 
 class Position:
@@ -100,6 +117,7 @@ class Position:
     SPRITE_WIDTH = 1
     SPRITE_HEIGHT = 1
 
-    def __init__(self, position_x, position_y):
-        self.position_x = position_x * self.SPRITE_WIDTH
-        self.position_y = position_y * self.SPRITE_HEIGHT
+    def __init__(self, thecolumn, therow):
+        print(thecolumn, therow)
+        self.thecolumn = thecolumn * self.SPRITE_WIDTH
+        self.therow = therow * self.SPRITE_HEIGHT
--- project3/main.py
+++ project3/main.py
@@ -5,7 +5,15 @@ import project3.data as p3
 
 
 def main():
-    current_level = p3.Background(0)
+    current_level = 0
+
+    while current_level == 0:
+        user_says = input("Ready Player 1 - Please enter 'y' to start "
+                          "playing")
+        if user_says == "y":
+            current_level += 1
+
+    p3.Background(current_level)
 
 
 if __name__ == '__main__':
