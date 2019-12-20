bca81a434a067fed41d4bef320cf69cde26efaf9
GERST Nathalie <NGERST@ad.edqm.eu>
2019-12-20 17:06:36 +0100

    Change names and reorganize

--- project3/data.py
+++ project3/data.py
@@ -2,7 +2,10 @@
 # coding: utf-8
 
 """
-:synopsis: [...]
+:synopsis: Classes and methods of people and objects on the playing board are
+contained in this file. The Background contains other classes' objects and
+is responsible for interactions between them.
+
 """
 
 # Standard lib imports
@@ -14,78 +17,95 @@ from random import randint
 import pygame as pg
 
 
-class Background:
+class Maze:
 
     WIDTH = 15
     HEIGHT = 15
+    SPRITE_WIDTH = 1
+    SPRITE_HEIGHT = 1
     FILE_LEVELS = "assets/levels.json"
-    VALUES = []
+    BOARD = []
+    ITEMS = []
+    HUMANS = []
 
     def __init__(self, level):
         self.level = "level" + str(level)
-        self.display_background(self.level)
-        self.aiguille = NewItem(self.randomize_position(),
-                                "https://cdn0.iconfinder.com/data/icons/world-issues/500/needle_and_thread-256.png")
-        self.tube = NewItem(self.randomize_position(),
-                            "https://cdn3.iconfinder.com/data/icons/medical-205/32/Artboard_10-256.png")
-        self.ether = NewItem(self.randomize_position(),
-                             "https://cdn3.iconfinder.com/data/icons/glypho-free/64/flask-256.png")
-        self.macguyver = Hero((0, 13), "https://user.oc-static.com/upload/2017/04/21/14927753100739_macgyver-32-43.png")
-        self.villain = Human((14, 6), "https://user.oc-static.com/upload/2017/04/21/14927753225921_murdoc-32.png")
+        self.read_values_from_json(self.level)
+        self.display_maze()  # self.level)
 
-    def display_background(self, values):
-        # values_pd = pd.DataFrame(self.read_values_from_json(values))
-        values = self.read_values_from_json(values)
-        # print(values_pd)
-        print(self.VALUES)
+        self.macgyver = Hero(self.HUMANS[0],
+                              "https://user.oc-static.com/upload/2017/04/21/14927753100739_macgyver-32-43.png")
+        self.guardian = Human(self.HUMANS[1],
+                             "https://user.oc-static.com/upload/2017/04/21/14927753225921_murdoc-32.png")
+        self.needle = Item(self.randomize_position(),
+                           "https://cdn0.iconfinder.com/data/icons/world-issues/500/needle_and_thread-256.png")
+        self.tube = Item(self.randomize_position(),
+                         "https://cdn3.iconfinder.com/data/icons/medical-205/32/Artboard_10-256.png")
+        self.ether = Item(self.randomize_position(),
+                          "https://cdn3.iconfinder.com/data/icons/glypho-free/64/flask-256.png")
+
+    def display_maze(self):  # , level_nb):
+        for line in self.BOARD:
+            print(line)
 
     def read_values_from_json(self, level_nb):
-        """ Retrieve background data from json file"""
+        """ Retrieve Maze data from json file"""
         # check if file exists
         try:
             open(self.FILE_LEVELS)
         except (OSError, IOError) as e:
-            self.VALUES.append("Cannot create background: no file "
-                               "found to create levels (error no.: " +
-                               str(e.errno) + ").")
+            self.BOARD.append("Cannot create Maze: no file "
+                              "found to create levels (error no.: " +
+                              str(e.errno) + ").")
         except Exception:
-            self.VALUES.append("Cannot create background, "
-                               "an unexpected error occurred:"
-                               + str(sys.exc_info()[0]))
+            self.BOARD.append("Cannot create Maze, "
+                              "an unexpected error occurred:"
+                              + str(sys.exc_info()[0]))
         else:
             # open a json file with my objects
             with open(self.FILE_LEVELS) as level:
-                self.VALUES = []
-                # load all the data contained in this file. data = entries
-                self.VALUES.append(json.load(level)[level_nb])
+                # load all the data contained in this file
+                self.BOARD.append(json.load(level)[level_nb]["humans"])
+                # self.HUMANS.append(json.load(level)[level_nb]["humans"])
+                print(self.BOARD)
+                # print(self.HUMANS)
 
-        return self.VALUES
+    # def position(self, thecolumn=randint(0, 14), therow=randint(0, 14)):
+    #
+    #     thecolumn = thecolumn * self.SPRITE_WIDTH
+    #     therow = therow * self.SPRITE_HEIGHT
+    #     return thecolumn, therow
 
     def randomize_position(self):
-        """ Sets a random position to Items MacGuyver needs to find """
-        column = randint(1, 15)
-        row = randint(1, 15)
-        while self.is_colliding("Item", (column, row)):
-            column = randint(1, 15)
-            row = randint(1, 15)
+        """ Sets a random position for Items MacGyver needs to find """
+        column = -1
+        row = -1
+        while self.BOARD[column][row] != "f" \
+                and (column != self.macgyver.position[0]
+                     or row != self.macgyver.position[1]) \
+                and (column != self.guardian.position[0]
+                     or row != self.guardian.position[1]):
+            column = randint(0, 14)
+            row = randint(0, 14)
         else:
+            self.ITEMS.append(column, row)
             return column, row
 
     def is_colliding(self, instance, position):
         """ Verifies if position of objects are relevant relative
-        to each other and background elements """
+        to each other and Maze elements """
         col = position[0] - 1
         row = position[1] - 1
         print(instance)
-        zone_type = self.VALUES[0][col][row]
+        zone_type = self.BOARD[col][row]
         return False
 
 
 class Human:
 
-    def __init__(self, position=(1, 1),
+    def __init__(self, position=(-1, -1),
                  image="https://avatars.dicebear.com/v2/male/joe.svg"):
-        self.is_alive = True
+        self.is_alive_and_kicking = True
         self.victory_phrase = "Yay"
         self.failure_phrase = "Argh..."
         self.position = position
@@ -100,7 +120,7 @@ class Hero(Human):
         self.moves = 0
 
 
-class NewItem:
+class Item:
 
     def __init__(self, position, image):
         self.position = position
@@ -108,16 +128,4 @@ class NewItem:
         self.display_item()
 
     def display_item(self):
-        pass
-        # print(self.position)
-
-
-class Position:
-
-    SPRITE_WIDTH = 1
-    SPRITE_HEIGHT = 1
-
-    def __init__(self, thecolumn, therow):
-        print(thecolumn, therow)
-        self.thecolumn = thecolumn * self.SPRITE_WIDTH
-        self.therow = therow * self.SPRITE_HEIGHT
+        print(self.position)
--- project3/main.py
+++ project3/main.py
@@ -8,12 +8,18 @@ def main():
     current_level = 0
 
     while current_level == 0:
-        user_says = input("Ready Player 1 - Please enter 'y' to start "
-                          "playing")
+        user_says = input("Ready Player 1 - Please enter 'y' to start playing")
         if user_says == "y":
             current_level += 1
+        else:
+            print("ok, no play for you then")
+            exit()
 
-    p3.Background(current_level)
+    p3.Maze(current_level)
+
+    # while p3.Maze.macgyver.is_alive_and_kicking:
+    #     direction = input("Where would you like to go? (u, d, l, r")
+    #     is_colliding()
 
 
 if __name__ == '__main__':
